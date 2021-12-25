aaee5a86bc225ad83011910a8ceaa0d68f45ea14
Vlad Boyko <swampman@inbox.ru>
2021-12-25 22:21:40 +0300

    test version

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import datetime
 import pytz
 import db
 
+userId = 0
+
 cntAll, cntYes, cntNo = 0, 0, 0
 ages = ['до 20', '20-29', '30-39', '40-49', '50-59', '60 + ']
 ageGroup = {ages[0]: '15', ages[1]: '25', ages[2]: '35', ages[3]: '45', ages[4]: '55', ages[5]: '65'}
@@ -45,22 +47,22 @@ bot = telebot.TeleBot(token=API_TOKEN)
 @bot.message_handler(content_types = ['text'])
 def get_text(message: types.Message):
     global ages
-    id = message.from_user.id
-    idname = db.check_id_name(id)
-    if idname is None:
-        keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-        key_15 = types.KeyboardButton(ages[0])
-        key_25 = types.KeyboardButton(ages[1])
-        key_35 = types.KeyboardButton(ages[2])
-        key_45 = types.KeyboardButton(ages[3])
-        key_55 = types.KeyboardButton(ages[4])
-        key_65 = types.KeyboardButton(ages[5])
-        keyboard.add(key_15, key_25, key_35, key_45, key_55, key_65)
-        msg = bot.send_message(message.chat.id, 'Независимый подсчет статистики по COVID-19\nУкажите вашу возрастную группу:', reply_markup=keyboard)
-        bot.register_next_step_handler(msg, age_button)
-    else:
-        bot.send_message(message.chat.id, f'Вы уже приняли участие в подсчете под ником {idname[1]}')
-        _send_stat(bot, message.chat.id)
+    #id = message.from_user.id
+    #idname = db.check_id_name(id)
+    #if idname is None:
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    key_15 = types.KeyboardButton(ages[0])
+    key_25 = types.KeyboardButton(ages[1])
+    key_35 = types.KeyboardButton(ages[2])
+    key_45 = types.KeyboardButton(ages[3])
+    key_55 = types.KeyboardButton(ages[4])
+    key_65 = types.KeyboardButton(ages[5])
+    keyboard.add(key_15, key_25, key_35, key_45, key_55, key_65)
+    msg = bot.send_message(message.chat.id, 'Независимый подсчет статистики по COVID-19\nУкажите вашу возрастную группу:', reply_markup=keyboard)
+    bot.register_next_step_handler(msg, age_button)
+    #else:
+    #    bot.send_message(message.chat.id, f'Вы уже приняли участие в подсчете под ником {idname[1]}')
+    #    _send_stat(bot, message.chat.id)
 
 # handle age answer
 def age_button(message: types.Message):
@@ -81,26 +83,27 @@ def age_button(message: types.Message):
 # write button data to DB
 @bot.callback_query_handler(func=lambda call: True)
 def answer(call: types.CallbackQuery):
-    global cntAll, cntYes, cntNo
-    id = call.from_user.id
+    global cntAll, cntYes, cntNo, userId
+    #id = call.from_user.id
     name = call.from_user.first_name
     if name is None:
         name = ""
-    if db.new_id(id):
-        ans = call.data.split(',')
-        print('insert id', id, 'name', name, 'age', ans[1], 'res', ans[0])
-        db.insert("user", {
-        "id": id,
-        "created": _get_now_formatted(),
-        "name": name,
-        "age": int(ans[1]),
-        "res": int(ans[0])
-        })
-        cntAll = db.count_users()[0]
-        cntYes = db.count_res()[0]
-        cntNo = cntAll - cntYes
-    else:
-        bot.send_message(call.message.chat.id, 'Произошла ошибка: повторный ввод')
+    #if db.new_id(id):
+    userId = userId + 1
+    ans = call.data.split(',')
+    print('insert id', userId, 'name', name, 'age', ans[1], 'res', ans[0])
+    db.insert("user", {
+    "id": userId,
+    "created": _get_now_formatted(),
+    "name": name,
+    "age": int(ans[1]),
+    "res": int(ans[0])
+    })
+    cntAll = db.count_users()[0]
+    cntYes = db.count_res()[0]
+    cntNo = cntAll - cntYes
+    #else:
+    #    bot.send_message(call.message.chat.id, 'Произошла ошибка: повторный ввод')
     _send_stat(bot, call.message.chat.id)
 
 
