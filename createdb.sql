4682e3b4abf3ef0b51165706781c9fba06fe608b
Vlad Boyko <swampman@inbox.ru>
2021-12-28 22:29:15 +0300

    [add] age index

--- createdb.sql
+++ createdb.sql
@@ -1,7 +1,9 @@
-create table user(
+create table if not exists user(
     id integer primary key,
     created datetime,
     name varchar(255),
     age integer,
     res integer
 );
+
+create index if not exists age_index on user (age);
